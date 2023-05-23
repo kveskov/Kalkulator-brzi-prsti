7ab530cce8344e245a1d2baf923847e94912fd13
kveskov <92784866+kveskov@users.noreply.github.com>
2023-05-23 12:12:41 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnpotvrdi = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnback = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btntocka = new System.Windows.Forms.Button();
+            this.btnnova = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblnetocni = new System.Windows.Forms.Label();
+            this.lbltocni = new System.Windows.Forms.Label();
+            this.lbltimer = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnpotvrdi
+            // 
+            this.btnpotvrdi.Location = new System.Drawing.Point(186, 83);
+            this.btnpotvrdi.Name = "btnpotvrdi";
+            this.btnpotvrdi.Size = new System.Drawing.Size(94, 29);
+            this.btnpotvrdi.TabIndex = 0;
+            this.btnpotvrdi.Text = "Потврди";
+            this.btnpotvrdi.UseVisualStyleBackColor = true;
+            this.btnpotvrdi.Click += new System.EventHandler(this.btnpotvrdi_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(386, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(34, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(34, 142);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(94, 29);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(186, 142);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(326, 142);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(186, 214);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(94, 29);
+            this.btn5.TabIndex = 6;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(326, 214);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(94, 29);
+            this.btn6.TabIndex = 7;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            this.btn6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn6_MouseClick);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(34, 260);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(94, 29);
+            this.btn7.TabIndex = 8;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(186, 265);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(94, 29);
+            this.btn8.TabIndex = 9;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(326, 265);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(94, 29);
+            this.btn9.TabIndex = 10;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // btnback
+            // 
+            this.btnback.Location = new System.Drawing.Point(34, 354);
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(94, 29);
+            this.btnback.TabIndex = 11;
+            this.btnback.Text = "<-";
+            this.btnback.UseVisualStyleBackColor = true;
+            this.btnback.Click += new System.EventHandler(this.btnback_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(34, 205);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(94, 29);
+            this.btn4.TabIndex = 12;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(186, 354);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(94, 29);
+            this.btn0.TabIndex = 13;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btntocka
+            // 
+            this.btntocka.Location = new System.Drawing.Point(326, 354);
+            this.btntocka.Name = "btntocka";
+            this.btntocka.Size = new System.Drawing.Size(94, 29);
+            this.btntocka.TabIndex = 15;
+            this.btntocka.Text = ".";
+            this.btntocka.UseVisualStyleBackColor = true;
+            this.btntocka.Click += new System.EventHandler(this.btntocka_Click);
+            // 
+            // btnnova
+            // 
+            this.btnnova.Location = new System.Drawing.Point(518, 71);
+            this.btnnova.Name = "btnnova";
+            this.btnnova.Size = new System.Drawing.Size(191, 29);
+            this.btnnova.TabIndex = 16;
+            this.btnnova.Text = "Почни нова игра";
+            this.btnnova.UseVisualStyleBackColor = true;
+            this.btnnova.Click += new System.EventHandler(this.btnnova_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 20);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Преостанато време";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 20);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Број на точни решенија";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 20);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Број на грешни решенија";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblnetocni);
+            this.groupBox1.Controls.Add(this.lbltocni);
+            this.groupBox1.Controls.Add(this.lbltimer);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(512, 133);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(412, 267);
+            this.groupBox1.TabIndex = 24;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Резултат";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lblnetocni
+            // 
+            this.lblnetocni.AutoSize = true;
+            this.lblnetocni.Location = new System.Drawing.Point(250, 200);
+            this.lblnetocni.Name = "lblnetocni";
+            this.lblnetocni.Size = new System.Drawing.Size(17, 20);
+            this.lblnetocni.TabIndex = 24;
+            this.lblnetocni.Text = "0";
+            this.lblnetocni.Click += new System.EventHandler(this.lblnetocni_Click);
+            // 
+            // lbltocni
+            // 
+            this.lbltocni.AutoSize = true;
+            this.lbltocni.Location = new System.Drawing.Point(250, 136);
+            this.lbltocni.Name = "lbltocni";
+            this.lbltocni.Size = new System.Drawing.Size(17, 20);
+            this.lbltocni.TabIndex = 23;
+            this.lbltocni.Text = "0";
+            this.lbltocni.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lbltimer
+            // 
+            this.lbltimer.AutoSize = true;
+            this.lbltimer.Location = new System.Drawing.Point(199, 72);
+            this.lbltimer.Name = "lbltimer";
+            this.lbltimer.Size = new System.Drawing.Size(33, 20);
+            this.lbltimer.TabIndex = 22;
+            this.lbltimer.Text = "180";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(328, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 21;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1309, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnnova);
+            this.Controls.Add(this.btntocka);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btnback);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnpotvrdi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnpotvrdi;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnback;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btntocka;
+        private System.Windows.Forms.Button btnnova;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbltimer;
+        private System.Windows.Forms.Label lblnetocni;
+        private System.Windows.Forms.Label lbltocni;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+
+  
+    public partial class Form1 : Form
+    {
+        int seconds = 0;
+        int poz=0;
+        int tocni = 0;
+        int netocni = 0;
+        string[] izraz1 = new string[] { "3+3", "3-2", "1+1", "4*3" ,"5+5","15-7*2"," 9*8/2","3+5*3/2","30-8*2","5*8-20","10-4*2"};
+        string[] izraz2 = new string[] { "6", "1", "2", "12" ,"10","1","36","9","14","20","2"};
+        string[] vnes = new string[100];
+        int pozvnez = 0;
+
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vnes[pozvnez] = "1";
+            pozvnez++;
+            for (int i = 0; i < pozvnez; i++)
+                
+                textBox2.Text = vnes[i];
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            vnes[pozvnez] = "2";
+            pozvnez++;
+            for (int i = 0; i < pozvnez; i++)
+                textBox2.Text = vnes[i];
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            vnes[pozvnez] = "3";
+            pozvnez++;
+            for (int i = 0; i < pozvnez; i++)
+                textBox2.Text = vnes[i];
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            vnes[pozvnez] = "4";
+            pozvnez++;
+            for (int i = 0; i < pozvnez; i++)
+                textBox2.Text = vnes[i];
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            vnes[pozvnez] = "7";
+            pozvnez++;
+            for (int i = 0; i < pozvnez; i++)
+                textBox2.Text = vnes[i];
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            vnes[pozvnez] = "9";
+            pozvnez++;
+            for (int i = 0; i < pozvnez; i++)
+                textBox2.Text = vnes[i];
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void btnnova_Click(object sender, EventArgs e)
+        {
+            seconds = 180;
+            timer1.Start();
+           
+            textBox1.Text = izraz1[0];
+            
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lbltimer.Text = seconds--.ToString();
+            if(seconds<0)
+                timer1.Stop();
+        }
+
+        private void btnpotvrdi_Click(object sender, EventArgs e)
+        {
+          
+
+            if (textBox2.Text == izraz2[poz])
+
+                tocni++;
+
+            else
+                netocni++;
+            lbltocni.Text=tocni.ToString();
+            lblnetocni.Text=netocni.ToString();
+            poz++;
+            textBox1.Text = izraz1[poz];
+            textBox2.Text = "";
+            pozvnez = 0;    
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblnetocni_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            vnes[pozvnez] = "5";
+            pozvnez++;
+            for (int i = 0; i < pozvnez; i++)
+                textBox2.Text = vnes[i];
+
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            vnes[pozvnez] = "6";
+            pozvnez++;
+            for (int i = 0; i < pozvnez; i++)
+                textBox2.Text = vnes[i];
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            vnes[pozvnez] = "8";
+            pozvnez++;
+            for (int i = 0; i < pozvnez; i++)
+                textBox2.Text = vnes[i];
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            vnes[pozvnez] = "0";
+            pozvnez++;
+            for (int i = 0; i < pozvnez; i++)
+                textBox2.Text = vnes[i];
+        }
+
+        private void btntocka_Click(object sender, EventArgs e)
+        {
+            vnes[pozvnez] = ".";
+            pozvnez++;
+            for (int i = 0; i < pozvnez; i++)
+                textBox2.Text = vnes[i];
+        }
+
+        private void btn6_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void btnback_Click(object sender, EventArgs e)
+        {
+            pozvnez--;
+            for (int i = 0; i < pozvnez; i++)
+                textBox2.Text = vnes[i];
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
